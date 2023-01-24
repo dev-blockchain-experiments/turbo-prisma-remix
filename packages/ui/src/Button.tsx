e0e167f0eec892e29eb94acb556e1f7a33089251
Matthew Trontz <mtrontz@outlook.com>
2023-01-23 21:50:43 -0500

    button dev

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,4 +1,46 @@
 import * as React from "react";
-export const Button = () => {
-  return <button>Boop op</button>;
+import {} from "@remix-run/react";
+
+const enum ActionTypes {
+  SUBMIT = "SUBMIT",
+  THEME = "THEME"
+}
+interface IButtonProps extends HTMLButtonElement {
+  buttonColor: string
+  buttonText: string 
 };
+
+interface IActionButtonProps extends IButtonProps {
+  action: keyof ActionTypes
+}
+export const RemixButton: React.FC<IButtonProps> = ({buttonColor, buttonText, ...props}) => {
+  return (
+  <button 
+  className={buttonColor}
+  ...props
+  >{buttonText}</button>
+  );
+};
+
+export const ActionButton: React.FC<IActionButtonProps> = ({ buttonColor, buttonText, action, ...props }) => {
+  const [clicked, setClicked] = React.useState(false);
+  React.useEffect(() => {
+    if (clicked === true) {
+      useSubmit(`/submit?action=${action}`, action);
+      setClicked(false);
+    }
+  }, [clicked]);
+
+  const handleClick: React.MouseEvent<HTMLButtonElement> = React.useCallback((e: React.MouseEvent) => {
+  e.preventDefault();
+  setClicked(true);
+  }, []);
+  return (
+    <RemixButton 
+    buttonColor={buttonColor}
+    buttonText={buttonText}
+    onClick={handleClick}
+    ...props
+    ></RemixButton>
+  )
+}
